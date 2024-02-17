f0d9083dec444755c7cbf7ca248d981414193661
claudio <rainclaudio25@gmail.com>
2024-02-17 16:27:11 -0300

    Half of section 5

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from 'react'
-import './App.css'
+import './styles.css'
 import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
+import { NavBar } from './NavBar';
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/activities')
+    axios.get<Activity[]>('http://localhost:5000/api/activities')
       .then(response => {
         setActivities(response.data)
       })
@@ -15,9 +17,10 @@ function App() {
 
   return (
     <>
-      <Header as='h2' icon='users' content='Reactivities'></Header>
+      <NavBar />
+
       <List>
-        {activities.map((activity:any) => (
+        {activities.map((activity) => (
           <List.Item key ={activity.id}>
             {activity.title}
           </List.Item>
