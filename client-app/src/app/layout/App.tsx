a5ad31ba383db01659b6dad63ae684d042141dc2
claudio <rainclaudio25@gmail.com>
2024-02-22 09:31:38 -0300

    End of Section 5

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,31 +1,71 @@
 import { useEffect, useState } from 'react'
 import './styles.css'
 import axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
+import {Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import { NavBar } from './NavBar';
+import { ActivityDashboard } from '../../features/activities/dashboard/ActivityDashboard';
+import {v4 as uuid} from 'uuid';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+  const [editMode, setEditMode] = useState(false); 
 
   useEffect(() => {
     axios.get<Activity[]>('http://localhost:5000/api/activities')
       .then(response => {
         setActivities(response.data)
       })
-  },[]);
+  },[])
+
+  function handleSelectActivity(id: string){
+    setSelectedActivity(activities.find(x => x.id === id))
+  }
+
+  function handleCancelSelectActivity(){
+    setSelectedActivity(undefined);
+  }
+
+  function handleFormOpen(id?:string){
+    id ? handleSelectActivity(id) : handleCancelSelectActivity();
+    setEditMode(true);
+  }
+
+  function handleFormClose(){
+    setEditMode(false);
+  }
+
+  function handleCreateOrEditActivity(activity: Activity){
+    activity.id 
+      ? setActivities([...activities.filter(x => x.id !== activity.id), activity ])
+      : setActivities([...activities, {...activity, id: uuid()}]);
+
+    setEditMode(false);
+
+    setSelectedActivity(activity);
+  }
+
+  function handleDeleteActivity(id: string){
+    setActivities([...activities.filter(x => x.id !== id)])
+  }
 
   return (
     <>
-      <NavBar />
-
-      <List>
-        {activities.map((activity) => (
-          <List.Item key ={activity.id}>
-            {activity.title}
-          </List.Item>
-        ))}
-      </List>
+      <NavBar openForm = {handleFormOpen} />
+      <Container style = {{marginTop: '7em'}}>
+        <ActivityDashboard 
+          activities = {activities} 
+          selectedActivity = {selectedActivity}
+          selectActivity = {handleSelectActivity}
+          cancelSelectActivity = {handleCancelSelectActivity}
+          editMode={editMode}
+          openForm={handleFormOpen}
+          closeForm={handleFormClose}       
+          createOrEdit={handleCreateOrEditActivity}
+          deleteActivity={handleDeleteActivity}
+        />
+      </Container>
     </>
   )
 }
